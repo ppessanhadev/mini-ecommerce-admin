f960c137bb30da4e7cc4f510a8c3b9df7302d530
Pablo Pessanha <pablopessanh@gmail.com>
2023-04-20 23:14:55 -0300

    feat: adds login screen

---
+++ components/Input.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    id: string;
+    label: string;
+    type: 'text' | 'password';
+    modelValue: string;
+    class?: string;
+    placeholder?: string;
+  }>();
+
+  defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <label :for="props.id" :class="`flex flex-col text-zinc-500 font-bold ${props.class}`">
+    {{ props.label }}
+    <input
+      :id="id"
+      :type="type"
+      :placeholder="props.placeholder"
+      :value="modelValue"
+      class="bg-gray-100 p-2 border font-normal"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    >
+  </label>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+  const fields = ref({ user: '', password: '' });
+
+  function mostrar() {
+    console.log(fields.value);
+  }
+</script>
+
 <template>
-  <h1>oi turu bom?</h1>
+  <Head>
+    <title>ME | Login</title>
+  </Head>
+
+  <main class="w-full h-screen bg-gray-100 flex justify-center content-center">
+    <form class="m-auto bg-white rounded-lg p-4 w-96 border shadow-md">
+      <Input id="user" v-model="fields.user" label="Usuário" type="text" placeholder="User" />
+      <Input id="password" v-model="fields.password" label="Senha" type="password" placeholder="Password" class="my-4" />
+
+      <button
+        type="button"
+        class="w-full block border-2 border-green-500 hover:bg-green-500 hover:text-white transition-all text-green-500 text-2xl font-bold p-2"
+        @click="mostrar"
+      >
+        Entrar
+      </button>
+    </form>
+  </main>
 </template>
