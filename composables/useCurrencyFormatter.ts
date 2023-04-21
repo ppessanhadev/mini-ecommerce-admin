67685bb320e53f5b52705bbabb35d4d3010268d9
Pablo Pessanha <pablopessanh@gmail.com>
2023-04-21 05:02:41 -0300

    feat: adds currency formatter

---
+++ composables/useCurrencyFormatter.ts
@@ -0,0 +1,10 @@
+
+const formatter = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+  minimumFractionDigits: 2
+});
+
+export function useCurrencyFormatter(value: number) {
+  return formatter.format(value);
+}
