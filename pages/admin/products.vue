46521a20429bbae606c0046aed77bd3696205fce
Pablo Pessanha <pablopessanh@gmail.com>
2023-04-21 14:02:31 -0300

    feat: adds new product button and head title

--- pages/admin/products.vue
+++ pages/admin/products.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  useHead({ title: 'MiniEcommerce | Produtos' });
   const auth = ref(false);
   // const { data: products } = useApi('/api/v1/product', {
   //   server: auth.value,
@@ -17,6 +18,9 @@
 <template>
   <main v-if="auth" class="w-auto">
     <section class="flex flex-col m-auto mt-20 w-screen lg:w-[960px] gap-4">
+      <button class="self-end text-white font-bold py-2 px-4 border-2 border-green-500 bg-green-400 hover:bg-green-500 transition-all">
+        Novo Produto
+      </button>
       <ProductCard />
       <ProductCard />
     </section>
